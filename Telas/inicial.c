df75f42fe2ccbb67bcefa8046ef62fba0327743f
Deoprag <pdroesofiarabelo@gmail.com>
2022-03-25 16:25:49 -0300

    Telas
    Criação de mais telas pro projeto

--- Telas/inicial.c
+++ Telas/inicial.c
@@ -32,7 +32,7 @@ int main()
     printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
     printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
     printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
-    printf("%c%c             \t[1]Login    \t[2]Cadastro\t             %c%c\n", 178, 178, 178, 178);
+    printf("%c%c             \t  [1]Login  \t[2]Cadastro\t             %c%c\n", 178, 178, 178, 178);
     printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
     printf("%c%c             \t     [3]Configuracoes    \t             %c%c\n", 178, 178, 178, 178);
     printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
@@ -47,32 +47,153 @@ int main()
     }
 
     opcaoinicial:
-
-    printf("\nDigite aqui a opcao desejada:");
+    
+    inicial = 0;
+    printf("\n\t\tDigite aqui a opcao desejada: ");
     scanf("%i", &inicial);
     
     if(inicial == 1){
-        goto login;
+
+        system("cls");
+        tijolo01, tijolo02, tijolo03 = 0;
+
+        for (tijolo01 = 0; tijolo01 < 63; tijolo01 ++){
+            printf("%c", 178);
+        }
+
+        printf("\n");
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c    222222   \t 0000000000 \t    4444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  2222222222 \t0000    0000\t   44444   \t88888888888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c          222\t000      000\t  44 444   \t888     888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c        22222\t00        00\t 44  444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c     22222   \t00        00\t44444444444\t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c   22222     \t000      000\t     444   \t888     888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  2222       \t0000    0000\t     444   \t88888888888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  22222222222\t 0000000000 \t     444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+
+        for (tijolo02 = 0; tijolo02 < 63; tijolo02 ++){
+            printf("%c", 178);
+        }
+
+        printf("\n");
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \tApelido: [             ] \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t  Senha: [             ] \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+
+        for (tijolo03 = 0; tijolo03 < 63; tijolo03 ++){
+            printf("%c", 178);
+        }
     } else if (inicial == 2){
-        goto cadastro;
+
+        system("cls");
+        tijolo01, tijolo02, tijolo03 = 0;
+
+        for (tijolo01 = 0; tijolo01 < 63; tijolo01 ++){
+            printf("%c", 178);
+        }
+
+        printf("\n");
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c    222222   \t 0000000000 \t    4444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  2222222222 \t0000    0000\t   44444   \t88888888888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c          222\t000      000\t  44 444   \t888     888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c        22222\t00        00\t 44  444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c     22222   \t00        00\t44444444444\t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c   22222     \t000      000\t     444   \t888     888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  2222       \t0000    0000\t     444   \t88888888888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  22222222222\t 0000000000 \t     444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+
+        for (tijolo02 = 0; tijolo02 < 63; tijolo02 ++){
+            printf("%c", 178);
+        }
+
+        printf("\n");
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \tApelido:         [             ] \t     %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \tSenha:           [             ] \t     %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \tConfirma Senha:  [             ] \t     %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \tCelular:         [             ] \t     %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+
+        for (tijolo03 = 0; tijolo03 < 63; tijolo03 ++){
+            printf("%c", 178);
+        }
+
     } else if (inicial == 3){
-        goto configuracoes;
+
+        system("cls");
+        tijolo01, tijolo02, tijolo03 = 0;
+
+        for (tijolo01 = 0; tijolo01 < 63; tijolo01 ++){
+            printf("%c", 178);
+        }
+
+        printf("\n");
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c    222222   \t 0000000000 \t    4444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  2222222222 \t0000    0000\t   44444   \t88888888888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c          222\t000      000\t  44 444   \t888     888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c        22222\t00        00\t 44  444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c     22222   \t00        00\t44444444444\t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c   22222     \t000      000\t     444   \t888     888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  2222       \t0000    0000\t     444   \t88888888888  %c%c\n", 178, 178, 178, 178);
+        printf("%c%c  22222222222\t 0000000000 \t     444   \t 888888888   %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+
+        for (tijolo02 = 0; tijolo02 < 63; tijolo02 ++){
+            printf("%c", 178);
+        }
+
+        printf("\n");
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t          IDIOMA:        \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t[1]Portugues\t [2]Ingles \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+        printf("%c%c             \t            \t           \t             %c%c\n", 178, 178, 178, 178);
+
+        for (tijolo03 = 0; tijolo03 < 63; tijolo03 ++){
+            printf("%c", 178);
+        }
+
+        
     } else {
-        printf("Escolha uma opcao valida!\n");
+        printf("\n\t\t%c Escolha uma opcao valida! %c\n", 174, 175);
         goto opcaoinicial;
     }
-
-    login:
-    printf("Sistema de login\n");
-    system ("pause");
-
-    cadastro:
-    printf("Sistema de cadastro\n");
-    system ("pause");
-    
-    configuracoes:
-    printf("Configuracoes\n");
-    system ("pause");
 
     return 0;
 }
